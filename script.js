191dc7967a29ae7011ef009b27caee97b584f959
adeliya21 <adeliya.salieva@gmail.com>
2023-09-06 18:24:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,51 +2,63 @@ document.addEventListener("DOMContentLoaded", function () {
     const listNameInput = document.getElementById("listName");
     const createListButton = document.getElementById("createList");
     const taskInput = document.getElementById("task");
-    const addTaskButton = document.getElementById("addTask");
-    const listsContainer = document.getElementById("lists");
+    const addTaskButton = document.getElementById("add");
+    const todoContainer = document.querySelector(".todo-container");
 
     createListButton.addEventListener("click", function () {
         const listName = listNameInput.value.trim();
         if (listName !== "") {
-            const list = document.createElement("div");
-            list.className = "todo-list";
-            list.innerHTML = `
+            const todoList = document.createElement("div");
+            todoList.className = "todo-list";
+            todoList.innerHTML = `
                 <h2>${listName}</h2>
-                <ul></ul>
+                <div class="input-container">
+                    <input type="text" class="task-input" placeholder="Add a new task">
+                    <button class="add-task">Add</button>
+                </div>
+                <ul class="task-list"></ul>
                 <button class="delete-list">Delete List</button>
             `;
-            listsContainer.appendChild(list);
+            todoContainer.appendChild(todoList);
             listNameInput.value = "";
-            addDeleteListListener(list);
+            addDeleteListListener(todoList);
+            addAddTaskListener(todoList);
         }
     });
 
     function addDeleteListListener(list) {
         const deleteListButton = list.querySelector(".delete-list");
         deleteListButton.addEventListener("click", function () {
-            listsContainer.removeChild(list);
+            todoContainer.removeChild(list);
         });
     }
 
-    addTaskButton.addEventListener("click", function () {
-        const taskText = taskInput.value.trim();
-        if (taskText !== "") {
-            const taskList = listsContainer.querySelector(".todo-list ul");
-            const li = document.createElement("li");
-            li.innerHTML = `
-                <span>${taskText}</span>
-                <span class="delete-task">Delete</span>
-            `;
-            taskList.appendChild(li);
-            taskInput.value = "";
-            addDeleteTaskListener(li);
-        }
-    });
+    function addAddTaskListener(list) {
+        const taskInput = list.querySelector(".task-input");
+        const addTaskButton = list.querySelector(".add-task");
+        const taskList = list.querySelector(".task-list");
 
-    function addDeleteTaskListener(li) {
-        const deleteButton = task.querySelector(".delete-task");
+        addTaskButton.addEventListener("click", function () {
+            const taskText = taskInput.value.trim();
+            if (taskText !== "") {
+                const li = document.createElement("li");
+                li.innerHTML = `
+                    <span>${taskText}</span>
+                    <span class="delete-task">Delete</span>
+                `;
+                taskList.appendChild(li);
+                taskInput.value = "";
+                addDeleteTaskListener(li);
+            }
+        });
+    }
+
+    function addDeleteTaskListener(task) {
+        const deleteTaskButton = task.querySelector(".delete-task");
         deleteTaskButton.addEventListener("click", function () {
+            const taskList = task.parentElement;
             taskList.removeChild(task);
         });
     }
 });
+
